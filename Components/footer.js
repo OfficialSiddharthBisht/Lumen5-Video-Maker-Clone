305000e95dc71a552c7dd5e6731a733f9a988855
Siddharth <officialsiddharthbisht@gmail.com>
2022-04-17 22:52:55 +0530

    imported footer using import export

---
+++ Components/footer.js
@@ -0,0 +1,50 @@
+function foot(){
+    return `
+    <div class="f-container">
+    <div class="footer-row">
+        <div class="footer-col">
+            <img src="Assets/Images/logo-white.png" alt="Lumen5-logo" width="80px" style="margin: 50px;">
+            <p id="footer-para">Lumen5 combines powerful A.I. with a simple drag-and-drop interface to help you create professional video content in minutes.</p>
+        </div>
+    </div>
+    <div class="footer-row">
+        <h4>EXPLORE</h4>
+        <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">LinkedIn</a></li>
+            <li><a href="#">Support</a></li>
+            <li><a href="#">Community group</a></li>
+        </ul>
+    </div>
+    <div class="footer-row hide-footer">
+        <h4>VIDEO TOOLS</h4>
+        <ul>
+            <li><a href="#">Add Text to Video</a></li>
+            <li><a href="#">Cut Video</a></li>
+            <li><a href="#">Merge Video</a></li>
+            <li><a href="#">Compress Video</a></li>
+            <li><a href="#">Resize Video</a></li>
+            <li><a href="#">See all</a></li>
+        </ul>
+    </div>
+    <div class="footer-row hide-footer">
+        <h4>VIDEO CREATORS</h4>
+        <ul>
+            <li><a href="#">Video Editor</a></li>
+            <li><a href="#">Photo Video Maker</a></li>
+            <li><a href="#">Facebook Video Maker</a></li>
+            <li><a href="#">YouTube Intro Maker</a></li>
+            <li><a href="#">Instagram Video Maker</a></li>
+            <li><a href="#">See All</a></li>
+        </ul>
+    </div>
+</div>
+<div class="end-note">
+    <p>Copyright © 2022 Lumen5</p>
+    <p>Terms of Use & Privacy Policy</p>
+</div>
+`
+}
+export default foot;
